11fb544fc418c5bcbc312b325e4fd0e1b3fccce7
Neeraj <nirajv217@gmail.com>
2025-11-15 05:48:47 +0530

    checking 1

--- backend/src/types/express-session.d.ts
+++ backend/src/types/express-session.d.ts
@@ -2,7 +2,7 @@ import "express-session";
 
 declare module "express-session" {
   interface SessionData {
-    Userid?: string;
-    user?: any;
+    userId?: string;
+    // add any custom session fields here
   }
 }
