ab385beafb1f46f7fe58aa98e0d3e5457ead2df4
Neeraj <nirajv217@gmail.com>
2025-11-15 14:09:14 +0530

    check11

--- backend/src/types/custom.d.ts
+++ backend/src/types/custom.d.ts
@@ -1,17 +1,17 @@
-// ---------------------------------------------------------
-// 1. cookie-parser has NO official types -> declare it
-// ---------------------------------------------------------
-declare module "cookie-parser";
+// // ---------------------------------------------------------
+// // 1. cookie-parser has NO official types -> declare it
+// // ---------------------------------------------------------
+// declare module "cookie-parser";
 
-// ---------------------------------------------------------
-// 2. express-session augmentation
-// ---------------------------------------------------------
-import "express-session";
+// // ---------------------------------------------------------
+// // 2. express-session augmentation
+// // ---------------------------------------------------------
+// import "express-session";
 
-declare module "express-session" {
-  interface SessionData {
-    Userid?: string;
-    user?: any;
-    passport?: any;
-  }
-}
+// declare module "express-session" {
+//   interface SessionData {
+//     Userid?: string;
+//     user?: any;
+//     passport?: any;
+//   }
+// }
